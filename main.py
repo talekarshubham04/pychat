988c2f75e0bd74b3fc0552d8bd15c0f47336d865
talekarshubham04 <shubhamtalekar33@gmail.com>
2024-06-16 20:10:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+import requests
+
+PRIVATE_KEY = "a5582eb3-2a9b-48e5-9438-4e1714cbde19"
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Private-Key": PRIVATE_KEY }
+    )
+    return response.json()
